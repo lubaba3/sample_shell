a9f56d045966c587213a3570b96fac91613c44fc
lubaba3 <lubabasalah520@gmail.com>
2023-12-10 10:25:30 +0200

    sample

--- shell.h
+++ shell.h
@@ -6,10 +6,28 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 #include <sys/stat.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+#define DELIM " \t\n"
+extern char **environ;
+
+char *read_line(void);
+
+char *_strdup(const char *str);
+int _strcmp(char *s1, char *s2);
+int _strlen(char *s);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+
+char **tokenizer(char *line);
+
+int _execute(char **command, char **argv);
+void freearray2D(char **arr);
+
 
 
 #endif
